ae123d6c0cecd9fe4580a5c81e08f735f8aead63
Jaime Huerta-Cepas <jhcepas@gmail.com>
2011-12-20 18:36:01 +0100

    fix node matching bug

--- matcher.py
+++ matcher.py
@@ -23,24 +23,35 @@ class CheckLeaf(object):
     def run(self, node):
         return node.is_leaf()
 
+isLeaf = CheckLeaf()
+
 def _parse_pattern(node, NHX_string):
     """ Reads node's extra data form its NHX string. NHX uses this
     format:  [&&NHX:prop1=value1:prop2=value2] """
     NHX_string = NHX_string.replace("[&&NHX:", "")
     NHX_string = NHX_string.replace("]", "")
-    for field in NHX_string.split(","):
+    is_leaf_check = False
+    for field in NHX_string.split(":"):
+        if field == "!":
+            is_leaf_check = True
+            continue
+           
 	ok = False
-	for key, (op, cast) in OPERATOR.iteritems(): 
+	for key, op in OPERATOR.iteritems(): 
 	    try:
 		attr, value = map(strip, field.split(key))
 	    except ValueError, e:
                 pass
 	    else:
-		node.conditions.append([attr, op, value])
+		node.constrain.append([attr, key, value])
 		ok = True
 		break
 	if not ok: 
 	    raise ValueError("Unknown operator [%s]" %field)
+
+    if is_leaf_check:
+        node.constrain.append(["isLeaf{}", "==", True])
+
 
 class TreePattern(Tree):
     def __init__(self, newick=None, format=0):
@@ -52,22 +63,68 @@ class TreePattern(Tree):
 	self.features = set([])
 	self.collapsed = False
 	# New for pattern type
-	self.conditions = []	    
+	self.constrain = []	    
 	if newick is not None:
 	    nwParser._parse_extra_features = _parse_pattern
 	    nwParser.read_newick(newick, root_node = self, format=format)
 
+
+    def __str__(self):
+        return self.get_ascii(show_internal="True")
+
+    def _asciiArt(self, char1='-', show_internal=True, compact=False):
+        """
+        Returns the ASCII representation of the tree. Code taken from the
+        PyCogent GPL project.
+        """
+        constrain = ", ".join(map(lambda x: "%s %s %s" %(x[0],x[1],x[2]), 
+                                 [(attr, op, value) for attr, op, value in self.constrain]))
+        name_txt = self.name + " (%s)" %constrain
+
+        LEN = 5
+        PAD = ' ' * LEN
+        PA = ' ' * (LEN-1)
+        if not self.is_leaf():
+            mids = []
+            result = []
+            for c in self.children:
+                if c is self.children[0]:
+                    char2 = '/'
+                elif c is self.children[-1]:
+                    char2 = '\\'
+                else:
+                    char2 = '-'
+                (clines, mid) = c._asciiArt(char2, show_internal, compact)
+                mids.append(mid+len(result))
+                result.extend(clines)
+                if not compact:
+                    result.append('')
+            if not compact:
+                result.pop()
+            (lo, hi, end) = (mids[0], mids[-1], len(result))
+            prefixes = [PAD] * (lo+1) + [PA+'|'] * (hi-lo-1) + [PAD] * (end-hi)
+            mid = (lo + hi) / 2
+            prefixes[mid] = char1 + '-'*(LEN-2) + prefixes[mid][-1]
+            result = [p+l for (p,l) in zip(prefixes, result)]
+            if show_internal:
+                stem = result[mid]
+                result[mid] = stem[0] + name_txt + stem[len(name_txt)+1:]
+            return (result, mid)
+        else:
+            return ([char1 + '-' + name_txt], 0)
+
 OPERATOR = {
-    ">=": [OP.ge, float], 
-    "<=": [OP.le, float],
-    ">": [OP.gt, float],
-    "<": [OP.lt, float],
-    "==": [OP.eq, float],
-    "~": [like, str],
+    ">=": OP.ge,
+    "<=": OP.le,
+    ">": OP.gt,
+    "<": OP.lt,
+    "==": OP.eq,
+    "~": like,
     }
 
 def _match(p, t):
-    for attr, op, expected in p.conditions: 
+    for attr, keyop, expected in p.constrain: 
+        op = OPERATOR[keyop]
         if attr.endswith("{}"):
             fn = globals()[attr.strip("{}")]
             value = fn.run(t)
@@ -81,10 +138,12 @@ def _match(p, t):
 def match(P, T):
     # Check expected features
     status = _match(P, T)
-    
+    #print P, T
+    #print "constrains", status
     # Enter into topology comparison
     matches = []
     if status and len(P.children):
+        #print "has children"
         if len(T.children) == len(P.children):
             # Check all possible comparison between patter children and
             # and tree node children.
@@ -94,13 +153,19 @@ def match(P, T):
                     st = match(ch, T.children[i]) 
                     sub_status &= st
                 status = sub_status
+                if status:
+                    break
         else:
+            #print "!= length"
             status = False
 
     return status
 
 
-pattern = "(([&&NHX:species==Mmu],([&&NHX:species==Hsa],[&&NHX:species==Hsa])),[&&NHX:species~Pt.]);"
+CountA = CountLeaves(lambda x: x.name == "A")
+
+
+pattern = "(([&&NHX:!:species==Mmu],([&&NHX:!:species==Hsa],[&&NHX:!:species==Hsa])),[&&NHX:!:species~Pt.]);"
 nw = "((Mms01, (Hsa01, Ptr01)), ((Mmu01, (Hsa01, Hsa02)),Ptr02), ((Mmu01, (Hsa01, Hsa02)),Pts02) );"
 
 
@@ -109,24 +174,20 @@ P = TreePattern(pattern)
 print P.write(features=[])
 
 T = PhyloTree(nw)
-
+print T, P
 for n in T.traverse():
     if match(P, n):
 	print "MATCHING NODE"
 	print n
+raw_input("continue")
 
-CountA = CountLeaves(lambda x: x.name == "A")
-isLeaf = CheckLeaf()
-pattern = "(any[&&NHX:isLeaf{}==1],moreA[&&NHX:CountA{}>=2]);"
+pattern = "(any, moreA[&&NHX:CountA{}>=1]);"
 
 P = TreePattern(pattern)
 print P
-for n in P.traverse():
-    print n.conditions
-print len(P.children)
 T = PhyloTree("(((A,B),(C,D)),U);")
 #print T
-T.populate(200, names_library="ABC", reuse_names=True)
+#T.populate(200, names_library="ABC", reuse_names=True)
 print T
 for n in T.traverse():
     if match(P, n):
