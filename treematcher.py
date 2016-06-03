019a25f81fae76d9e380e0870ec7d0a593d0bf50
Alisha Mechtley <acr11n@my.fsu.edu>
2016-06-03 01:34:46 -0400

    Syntax keywords moved from dictionary function to class variable list. Introduced @.children and  improved tests.

--- treematcher.py
+++ treematcher.py
@@ -1,5 +1,4 @@
 from itertools import permutations
-from collections import OrderedDict
 from string import strip
 import re
 import sys
@@ -7,6 +6,23 @@ import sys
 from ete3 import PhyloTree, Tree
 
 class TreePattern(Tree):
+
+    _syntax_tuples = [
+        ("@", "__target"),
+        ("__target.leaves", "[node.name for node in __target.get_leaves()]"),
+        ("__target.children", "[n.name for n in __target.children]"),
+        ("is greater than", ">"),
+        ("greater than", ">"),
+        (" or equal to", "="),
+        ("is less than", "<"),
+        ("less than", "<"),
+        ("Children", "[n.name for n in __target.children]"),
+        ("Distance", "__target.dist"),
+        ("Leaves", "[node.name for node in __target.get_leaves()]"),
+        ("Species", "__target.species"),
+        (" is ", " == "),
+        ]
+
     def __init__(self, *args, **kargs):
         kargs["format"] = 1
         Tree.__init__(self, *args, **kargs)
@@ -62,31 +78,13 @@ class TreePattern(Tree):
                 return True, node
         return False, None
 
-    def _make_syntax_dictionary(self):
-
-        syntax_dict = OrderedDict([
-            ("@", "__target"),
-            ("Leaves", "[node.name for node in __target.get_leaves()]"),
-            ("__target.leaves", "[node.name for node in __target.get_leaves()]"),
-            ("Distance", "__target.dist"),
-            ("Species", "__target.species"),
-            ("less than", "<"),
-            (" or equal to", "="),
-            ("greater than", ">"),
-            (" is ", " == "),
-        ])
-        return syntax_dict
-
-
     def _parse_constraint(self,node):
-
-        syntax_dict = self._make_syntax_dictionary()
 
         node.constraint = node.name
         # use regular expressions turn multiple spaces to single space
         node.constraint = re.sub("\s+", " ", node.constraint)
 
-        for keyword, python_code in syntax_dict.items():
+        for keyword, python_code in self._syntax_tuples:
             try:
                 node.constraint = node.constraint.replace(keyword, python_code)
             except (KeyError, ValueError):
@@ -94,21 +92,19 @@ class TreePattern(Tree):
 
         return
 
-
 def length(txt):
     return len(txt)
 
-
 def test_basic():
-    custom_functions = {"length":length}
-
-
+    """
+        tests basic ete parameters are working
+    """
     pattern = """
-        (
-        'len(@.children) > 2 and @.name in ("hello","bye") '
-        )
-        '(length(@.name) < 3) and @.dist >= 0.5';
-        """
+            (
+            'len(@.children) > 2 and @.name in ("hello","bye") '
+            )
+            '(len(@.name) < 3) and @.dist >= 0.5';
+            """
 
     pattern = TreePattern(pattern, format=8, quoted_node_names=True)
 
@@ -116,22 +112,60 @@ def test_basic():
 
     tree = Tree("(hello,(1,2,3)kk)pasa:1;", format=1)
     print tree.get_ascii(attributes=["name", "dist"])
-    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+    print "Pattern matches tree?:", pattern.find_match(tree, None)
 
     tree = Tree("((kk,(1,2,3)bye)y:1, NODE);", format=1)
     print tree.get_ascii(attributes=["name", "dist"])
-    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+    print "Pattern matches tree?:", pattern.find_match(tree, None)
 
-    tree = Tree("(((1,2,3)bye)y:1, NODE);", format=1)
+    tree = Tree("(((1,2,3)bye)y:1, NoName);", format=1)
     print tree.get_ascii(attributes=["name", "dist"])
-    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
-
-    tree = Tree("(((1,2,3)bye,kk)y:1, NODE);", format=1)
-    print tree.get_ascii(attributes=["name", "dist"])
-    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+    print "Pattern matches tree?:", pattern.find_match(tree, None)
 
 
 def test_syntax():
+    """
+        tests syntax-to-python conversion is working
+    """
+    # " Saccharomyces_cerevisiae_1" in Children
+    # "Homo_sapiens_1" in @.children
+
+    pattern1 = """
+        ( '  @.dist >= 0.5 ' , ' @.dist<2  ')
+        '    "Pan_troglodytes_1" in @.leaves and "Homo_sapiens_1" in @.children '
+        ;
+        """
+
+    pattern2 = """
+        ( '  Distance greater than or equal to 0.5 ' , ' Distance less than 2 ' )
+        '     "Pan_troglodytes_1" in Leaves and "Homo_sapiens_1" in Children'
+        ;
+        """
+
+    pattern1 = TreePattern(pattern1, format=8, quoted_node_names=True)
+    pattern2 = TreePattern(pattern2, format=8, quoted_node_names=True)
+
+    print pattern1
+    print pattern2
+
+
+    tree = PhyloTree("((((Anolis_carolinensis_1:1, Gallus_gallus_1:1), (Felis_catus_1:1, (Homo_sapiens_1:1, Pan_troglodytes_1:1))), ((Danio_rerio_1:1, (Xenopus_laevis_1:1, Anolis_carolinensis_1:1)), Saccharomyces_cerevisiae_2:1)), Saccharomyces_cerevisiae_1:1);", format=1)
+    tree.set_species_naming_function(lambda n: n.name.split("_")[1] if "_" in n.name else '')
+    print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree?:", pattern1.find_match(tree, None)
+    print "Pattern without symbols matches tree?:", pattern2.find_match(tree, None)
+
+
+    tree = PhyloTree("((((Anolis_carolinensis_1:1, Gallus_gallus_1:1), (Felis_catus_1:1, (Homo_sapiens_1:1, Pan_troglodytes_2:1))), ((Danio_rerio_1:1, (Xenopus_laevis_1:1, Anolis_carolinensis_1:1)), Saccharomyces_cerevisiae_2:1)), Saccharomyces_cerevisiae_1:1);", format=1)
+    tree.set_species_naming_function(lambda n: n.name.split("_")[1] if "_" in n.name else '')
+    #print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree missing leaf?:", pattern1.find_match(tree, None)
+    print "Pattern without symbols matches tree missing leaf?:", pattern2.find_match(tree, None)
+
+def test_species():
+    """
+        tests if @.species is working
+    """
 
     pattern1 = """
         (
@@ -164,24 +198,64 @@ def test_syntax():
     tree.set_species_naming_function(lambda n: n.name.split("_")[1] if "_" in n.name else '')
     print tree.get_ascii(attributes=["species", "dist"])
     print "Pattern matches tree?:", pattern1.find_match(tree, None)
-    print "Pattern without @ matches tree?:", pattern2.find_match(tree, None)
+    print "Pattern without symbols matches tree?:", pattern2.find_match(tree, None)
 
     tree = PhyloTree("((((Anolis_carolinensis_1:1, Gallus_gallus_1:1), (Felis_catus_1:1, (Homo_sapiens_1:1, Pan_troglodytes_1:1))), ((Danio_rerio_1:1, (Xenopus_laevis_1:1, Anolis_carolinensis_1:1)), Saccharomyces_cerevisiae_2:1)), Saccharomyces_cerevisiae_1:1);", format=1)
     tree.set_species_naming_function(lambda node: (node.name.split("_")[0] + " " + node.name.split("_")[1]) if "_" in node.name else '')
-    print tree.get_ascii(attributes=["name", "dist"])
+    #print tree.get_ascii(attributes=["name", "dist"])
     print "Pattern matches wrong species tree?:", pattern1.find_match(tree, None)
-    print "Pattern without @ matches wrong species tree?:", pattern2.find_match(tree, None)
+    print "Pattern without symbols matches wrong species tree?:", pattern2.find_match(tree, None)
 
     tree = PhyloTree("((((Anolis_carolinensis_1:1, Gallus_gallus_1:1), (Felis_catus_1:1, (Homo_sapiens_1:1, Pan_troglodytes_2:1))), ((Danio_rerio_1:1, (Xenopus_laevis_1:1, Anolis_carolinensis_1:1)), Saccharomyces_cerevisiae_2:1)), Saccharomyces_cerevisiae_1:1);", format=1)
     tree.set_species_naming_function(lambda n: n.name.split("_")[1] if "_" in n.name else '')
-    print tree.get_ascii(attributes=["name", "dist"])
+    #print tree.get_ascii(attributes=["name", "dist"])
     print "Pattern matches tree missing leaf?:", pattern1.find_match(tree, None)
-    print "Pattern without @ matches tree missing leaf?:", pattern2.find_match(tree, None)
+    print "Pattern without symbols matches tree missing leaf?:", pattern2.find_match(tree, None)
+
+def test_custom_functions():
+    """
+        tests all custom functions are working
+    """
+    custom_functions = {"length":length}
+
+
+    pattern = """
+        (
+        'len(@.children) > 2 and @.name in ("hello","bye") '
+        )
+        '(length(@.name) < 3) and @.dist >= 0.5';
+        """
+
+    pattern = TreePattern(pattern, format=8, quoted_node_names=True)
+
+    print pattern
+
+    tree = Tree("(hello,(1,2,3)kk)pasa:1;", format=1)
+    print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+
+    tree = Tree("((kk,(1,2,3)bye)y:1, NODE);", format=1)
+    print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+
+    tree = Tree("(((1,2,3)bye)y:1, NODE);", format=1)
+    print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+
+    tree = Tree("(((1,2,3)bye,kk)y:1, NODE);", format=1)
+    print tree.get_ascii(attributes=["name", "dist"])
+    print "Pattern matches tree?:", pattern.find_match(tree, custom_functions)
+
 
 
 if __name__ == "__main__":
+
     test_basic()
     test_syntax()
+    test_species()
+    #test_custom_functions()
+    #test_evol_events()
+
 
 
 ################################
@@ -194,13 +268,12 @@ if __name__ == "__main__":
     #    @.species == "sapiens"or node.name=="pygmaeus"
     #    which may not be what people expect
     # 2) @.species will fail if not all nodes have species
+    #
+    # 3) if someone were to name their leaves with keywords, they would be modified
+    #   need to replace inner strings with temporary variables during the replace
 
 
     ##### To Do ######
-    #
-    # parse strings using regular expressions
-    #
-    # use keyword Subtree to apply to all nodes children
     #
     # .replace("@.lineage","__target.get_taxonomy")
     # .replace("Lineage", "[(ncbi.get_taxid_translator(lineage))[taxid] for taxid in ncbi.get_lineage(__target.taxid)]")\
@@ -216,3 +289,5 @@ if __name__ == "__main__":
     #
     # when parsing constraints
     # may want to pull out everything in quotes first or extract some python keywords (in, is, or, and) first?
+    #
+    # add regular expressions to each keyword as needed
