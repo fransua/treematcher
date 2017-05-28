d71ab0c2d7b2f1861e05f3462cc532a450cedd3b
dpliakos <dpliakos@linux-z0o8.suse>
2017-05-28 12:50:45 +0300

    fix typo symbols.py

--- symbols.py
+++ symbols.py
@@ -3,10 +3,10 @@ SYMBOL = {
 	'is_root': '^',
 	'is_leaf': '$',
 	'zero_or_more': '*',
-	'one_or_node': '+',
-	'zero_or_one': '?'	
-<<<<<<< HEAD
+	'one_or_more': '+',
+	'zero_or_one': '?'
+#<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 68441ea7045d8fc667298d6910cc57e309313887
+#=======
+#}
+#>>>>>>> 68441ea7045d8fc667298d6910cc57e309313887
